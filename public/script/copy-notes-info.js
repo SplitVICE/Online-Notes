7d83710d9b36d4d400c2f50edbf9dc9a272ec19e
JustVice <43510837+JustVice@users.noreply.github.com>
2020-11-01 18:32:10 -0600

    Copy title fix - note date is no longer also copied.

--- public/script/copy-notes-info.js
+++ public/script/copy-notes-info.js
@@ -1,7 +1,7 @@
 function copyTitleToClipboard(id) {
     note_title_id = "note_title_" + id
     const title = document.getElementById(note_title_id).innerText
-    copy_text_to_clipboard(title);
+    copy_text_to_clipboard(extractTitle(title));
     sweetAlert_success("Title copied into clipboard");
 }
 
@@ -10,4 +10,20 @@ function copyDescriptionToClipboard(id) {
     const desc = document.getElementById(note_description_id).innerText
     copy_text_to_clipboard(desc);
     sweetAlert_success("Description copied into clipboard");
+}
+
+/**
+ * Titles are shown on canvas as [title] - [date]. This function
+ * recibes a title with this format and returns only [title].
+ * @param {string} input Title input to extract actual title.
+ */
+function extractTitle(input){
+    const regex = /^.*(?=(\ - 202))/gm;
+    let regexResult;
+    while ((regexResult = regex.exec(input)) !== null) {
+        if (regexResult.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+        return regexResult[0];
+    }
 }
